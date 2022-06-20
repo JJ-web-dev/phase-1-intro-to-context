ae8f510848ac3fed16fa17b800512ccf8752cc82
Justin James <justinmjames@protonmail.com>
2022-06-20 16:50:08 -0700

    commit tree 7c9c1e3dba9a570ddb31dd6e3ac2c22862d57b78

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+
+function createEmployeeRecord(employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+
+    }
+};
+
+function createEmployeeRecords(arrayArrays) {
+    return arrayArrays.map(employee => createEmployeeRecord(employee))
+};
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+    const [date, hour] = dateStamp
+    
+    const eventObj = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    }
+    employeeRecord.timeInEvents.push(eventObj);
+    
+    
+    // const [date, hour] = dateStamp.split(' ')
+    // const eventObj = {
+    //     type: "TimeIn",
+    //     hour: parseInt(hour, 10),
+    //     date: date
+    // }
+
+    // this.timeInEvents.push(eventObj)
+    // return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ')
+    const eventObj = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    }
+    
+    this.timeOutEvents.push(eventObj)
+    return this
+}
+
+function hoursWorkedOnDay() {
+    
+}
